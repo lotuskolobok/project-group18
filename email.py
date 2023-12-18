b0d427e33838c8369d0c6e5e8568b69b451bbe5b
Elizaveta Psar <105010678+Psar-Elizaveta@users.noreply.github.com>
2023-12-18 18:46:22 +0200

    Update email.py

--- email.py
+++ email.py
@@ -1,20 +1,19 @@
 import re
 
+class Email(Field):
+    def __str__(self):
+        return self.value
+    
+    @property
+    def email_value(self):
+        return self._Field__value
 
-class Email:
-    def __init__(self, value=''):
-        self.values = []
-        if value:
-            self.value = value
-        else:
-            self.value = input('Enter email: ')
-        self.validate_email()
+    @email_value.setter
+    def email_value(self, value):
+        self._Field__value = self.validate_email(value)
         
-    def validate_email(self):
-        try:
-            if re.match(r'^[\w]{1,}([\w.+-]{0,1}[\w]{1,}){0,}@[\w]{1,}([\w-]{0,1}[\w]{1,}){0,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$', self.value):
-                self.values.append(self.value)
-            else:
-                raise ValueError
-        except ValueError:
-            print('Invalid email! Please enter correct email')
+    def validate_email(self, value):
+        if re.match(r'^[\w]{1,}([\w.+-]{0,1}[\w]{1,}){0,}@[\w]{1,}([\w-]{0,1}[\w]{1,}){0,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$', self.value):
+            return value
+        else:
+            raise ValueError('Invalid email address! Please enter correct email')
