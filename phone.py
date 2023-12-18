522617cb36ea146d6abd13cff5611b38f13dcbbb
Elizaveta Psar <105010678+Psar-Elizaveta@users.noreply.github.com>
2023-12-18 18:38:08 +0200

    Update phone.py

--- phone.py
+++ phone.py
@@ -1,31 +1,31 @@
 import re
 
-class Phone:
-    def __init__(self, value=''):
-        self.values = []
-        if value:
-            self.value = value
-        else:
-            self.value = input('Enter phone: ')
-        self.validate_phone()
+class Phone(Field):
+    def __str__(self):
+        return self.value
+    
+    @property
+    def value(self):
+        return self._Field__value
 
-    def validate_phone(self):
-        try:
-            phone_number = (self.value.strip()
+    @value.setter
+    def value(self, value):
+        self._Field__value = self.validate_phone(value)
+
+    def validate_phone(self, value):
+        phone_number = (value.strip()
                         .replace('(', '')
                         .replace(')', '')
                         .replace('-', '')
-                        .replace(' ', ''))
-            if len(phone_number)== 13:
-                if re.match('^\\+38\d{10}$', phone_number):
-                    self.values.append(phone_number)
-            elif len(phone_number) == 12:
-                if re.match('^\d{12}$', phone_number):
-                    self.values.append('+' + phone_number)
-            elif len(phone_number) == 10:
-                if re.match('^\d{10}$', phone_number):
-                    self.values.append('+38' + phone_number)
-            else:
-                raise ValueError
-        except ValueError:
-            print('Invalid phone number! Please enter correct number phone!')
+                        .replace(' ', '')
+                        .replace('+', ''))
+        if len(phone_number)== 13:
+            if re.match('^\\+38\d{10}$', phone_number):
+                return phone_number
+        elif len(phone_number) == 12:
+            if re.match('^\d{12}$', phone_number):
+                return '+' + phone_number
+        elif len(phone_number) == 10:
+            if re.match('^\d{10}$', phone_number):
+                return '+38' + phone_number
+        raise ValueError('Invalid phone number! Please enter correct number phone!')
