72e0a9a9294481f668601349caf5d1d39d1ebbe9
lotuskolobok <142893815+lotuskolobok@users.noreply.github.com>
2023-12-22 18:23:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from datetime import datetime
 from datetime import date
 
 import re
+import pickle
+import cmd
 
-def phone_validate(phone_number: str):
+def validate_email(email):
+    try:
+        if re.match(r'^[\w]{1,}([\w.+-]{0,1}[\w]{1,}){0,}@[\w]{1,}([\w-]{0,1}[\w]{1,}){0,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$', email):
+            return True
+        else:
+            raise ValueError
+    except:
+        print('Invalid email address! Please enter correct email')
+        return False
+
+
+def validate_phone(phone_number: str):
     
     try:
         phone_number = (phone_number.strip()
@@ -76,15 +89,23 @@ class Birthday(Field):
         except ValueError:
             return None
 
+
+class Email(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+    def __str__(self):
+        return self.value
+
 class Record:
-    def __init__(self, name, birthday=None, email=None):
+    def __init__(self, name, birthday=None):
         self.name = Name(name)
         self.phones = []
         self.birthday = Birthday(birthday)
-        self.email = Email(email)
+        self.emails = []
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}, email: {self.email.value}"
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}, e-mails: {'; '.join(p.value for p in self.emails)}"
 
     def show_birthday(self):
         return f"Contact name: {self.name.value}, birthday: {self.birthday.value}"
@@ -115,15 +136,42 @@ class Record:
         else:
             return ''
 
+    def add_email(self, email: str):
+        tmp = validate_email(email)
+        if tmp:
+            self.emails.append(Email(email))
+        else:
+            print(f'E-mail {email} is not valid.')
+    
+    def find_email(self, email: str):
+        
+        for e in self.emails:
+            if e.value == email:
+                return e
+
+        return None
+    
+    def edit_email(self, old_email, new_email):
+        email_obj = self.find_email(old_email)
+
+        if email_obj:
+            if validate_email(new_email):
+                email_obj.value = new_email
+            return True
+        else:
+            print(f'E-mail {old_email} not found.')
+            return False
+    
+
     def add_phone(self, phone_number: str):
-        tmp = phone_validate(phone_number)
+        tmp = validate_phone(phone_number)
         if tmp:
             self.phones.append(Phone(tmp))
         else:
             print(f'Number {phone_number} is not valid.')
 
     def find_phone(self, phone_number: str):
-        phone_number = phone_validate(phone_number)
+        phone_number = validate_phone(phone_number)
 
         for phone in self.phones:
             if phone.value == phone_number:
@@ -132,46 +180,22 @@ class Record:
         return None
 
     def edit_phone(self, old_phone, new_phone):
-        old_phone = phone_validate(old_phone)
+        old_phone = validate_phone(old_phone)
         phone_obj = self.find_phone(old_phone)
         
         if phone_obj:
-            new_phone = phone_validate(new_phone)
+            new_phone = validate_phone(new_phone)
             phone_obj.value = new_phone
             return True
         else:
+            print(f'Number {old_phone} not found.')
             return False
 
     def remove_phone(self, rem_phone):
         for phone in self.phones:
             if phone.value == rem_phone:
                 self.phones.remove(phone)
-
-
-class Email(Field):
-    def __str__(self):
-        return self.value
-
-    @property
-    def email_value(self):
-        return self._Field__value
-
-    @email_value.setter
-    def email_value(self, value):
-        e = self.validate_email(value)
-        if e:
-            self._Field__value = value
-        
-    def validate_email(self, value):
-        
-        try:
-            if re.match(r'^[\w]{1,}([\w.+-]{0,1}[\w]{1,}){0,}@[\w]{1,}([\w-]{0,1}[\w]{1,}){0,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$', self.value):
-                return True
-            else:
-                raise ValueError
-        except:
-            print('Invalid email address! Please enter correct email')
-            return False
+    
 
 class AddressBook(UserDict):
 
@@ -189,33 +213,65 @@ class AddressBook(UserDict):
         if name in self.data:
             del self.data[name]
 
-    # пошук записів за збігом в імені або номері телефона
+    
     def find_record(self, part: str):
         result = {}
 
         for item, record in self.items():
-            # пошук в номері телефона
+            # пошук в phones
             for p in record.phones:
                 if part in str(p):
                     result[item] = self.data[item]
 
-            # пошук в імені
+            # пошук в name
             if part.lower() in item.lower():
                 result[item] = self.data[item]
+            
+            # пошук в emails
+            for e in record.emails:
+                if part in str(e):
+                    result[item] = self.data[item]
 
         return result
+    
+    # сериалізація адресної книги та запису її у файл
+    def dump(self):
+        with open('my_book.txt', 'wb') as file:
+            if len(self.data) > 0:
+                pickle.dump(self, file)
+                return True
+
+    # десериалізація адресної книги з файла
+    def load(self):
+        try:
+            with open('my_book.txt', 'rb') as file:
+                self = pickle.load(file)
+                
+        except:
+            pass
+    
+    # сериалізація адресної книги при виході з програми
+    def exit(self):
+        result = self.dump()
+        return result
+        
+
 # ----------------------------------------------------------------------------------------------------------
 
 
-if __name__ == "__main__":
+def Examle():
 
-    print ('*' * 100)
-    
+    print('*' * 100)
+
     # Створення нової адресної книги
     book = AddressBook()
 
     # Створення запису для John
-    john_record = Record("John", "2000-10-05", "bobik@dog.yes")
+    john_record = Record("John", "2000-10-05")
+
+    john_record.add_email("bobik@dog.yes")
+    john_record.add_email("murzik@cat.yes")
+
     john_record.add_phone("1234567890")
     john_record.add_phone("5555555555")
 
@@ -224,6 +280,7 @@ if __name__ == "__main__":
 
     # Створення та додавання нового запису для Jane
     jane_record = Record("Jane", "200002-20")
+    jane_record.add_email("dfdf")
     jane_record.add_phone("9876543210")
     book.add_record(jane_record)
 
@@ -234,12 +291,16 @@ if __name__ == "__main__":
     sara_record.birthday = Birthday("1976-07-14")
 
     # Виведення всіх записів у книзі
-    for name, record in book.data.items():
+    for record in book.data.items():
         print(record)
 
     # Знаходження та редагування телефону для John
     john = book.find("John")
     john.edit_phone("1234567890", "1112223333")
+
+    # Знаходження та редагування email для John
+    john = book.find("John")
+    john.edit_email("bobik@dog.yes", "sharik@dog.yes")
 
     print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
@@ -261,4 +322,50 @@ if __name__ == "__main__":
     print(sara_record.show_birthday(), sara_record.days_to_birthday())
 
     # Видалення запису Jane
-    book.delete("Jane")
+    # book.delete("Jane")
+
+    book.exit()
+
+if __name__ == "__main__":
+
+    #Examle()
+
+    while True:
+        my_book = None
+
+        my_book = AddressBook().load()
+        #book = book.load()
+
+        command = input('Input command or "?" for help: ')
+
+        if command.lower() == "end":
+            print(my_book.exit())
+            break
+        
+        elif command == 'add_user':
+            name = input('    Input name: ')
+            if name != '':
+                birthday = input('    Input bithday in format [yyyy-mm-dd]: ')
+                record = Record(name, birthday)
+            else:
+                print("Name can't be empty. Try again.")
+        
+        elif command == 'show_book':
+            # Виведення всіх записів у книзі
+            if my_book:
+                for record in my_book.data.items():
+                    print(record)
+
+        
+        elif command == "?":
+            commands = []
+            commands.append('end')
+            commands.append('add_user (name, [birthday]])')
+            
+
+            for c in commands:
+                print(f'{c}')
+
+
+
+    
